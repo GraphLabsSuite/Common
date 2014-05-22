c25bc998e8d202292e63aa0d08f182dd5a069f60
Святослав Захряпин <svt.zhr@gmail.com>
2014-05-22 09:16:30 +0000

    guid сессии в параметрах запуска задания
    git-svn-id: https://svtz.ru/svn/graphlabs.common/trunk@12 fbe919e9-c1b6-4f85-a809-c73955cdc020

--- GraphLabs.Tasks.Contract/InitParams.cs
+++ GraphLabs.Tasks.Contract/InitParams.cs
@@ -1,4 +1,6 @@
-﻿namespace GraphLabs.Tasks.Contract
+﻿using System;
+
+namespace GraphLabs.Tasks.Contract
 {
     /// <summary> Параметры инициализации модуля-задания </summary>
     public class InitParams
@@ -6,12 +8,14 @@
         public long TaskId { get; set; }
         public long? VariantId { get; set; }
         public long LabId { get; set; }
+        public Guid SessionGuid { get; set; }
 
-        private InitParams(long taskId, long? variantId, long labId)
+        private InitParams(long taskId, long? variantId, long labId, Guid sessionGuid)
         {
             TaskId = taskId;
             VariantId = variantId;
             LabId = labId;
+            SessionGuid = sessionGuid;
         }
 
         /// <summary> Чтобы сериализация работала </summary>
@@ -20,15 +24,15 @@
         }
 
         /// <summary> Для контрольного режима </summary>
-        public InitParams ForControlMode(long taskId, long labId)
+        public InitParams ForControlMode(Guid sessionGuid, long taskId, long labId)
         {
-            return new InitParams(taskId, null, labId);
+            return new InitParams(taskId, null, labId, sessionGuid);
         }
 
         /// <summary> Для ознакомительного режима </summary>
-        public InitParams ForDemoMode(long taskId, long variantId, long labId)
+        public InitParams ForDemoMode(Guid sessionGuid, long taskId, long variantId, long labId)
         {
-            return new InitParams(taskId, variantId, labId);
+            return new InitParams(taskId, variantId, labId, sessionGuid);
         }
     }
 }
