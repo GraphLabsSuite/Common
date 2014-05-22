3cbcdc100e849fea6d0bc4103e6c198542e6c788
Святослав Захряпин <svt.zhr@gmail.com>
2014-05-22 04:11:22 +0000

    забыл про структуру каталогов
    git-svn-id: https://svtz.ru/svn/graphlabs.common/trunk@3 fbe919e9-c1b6-4f85-a809-c73955cdc020

---
+++ GraphLabs.Common.Utils/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("GraphLabs.Common.Utils")]
+[assembly: AssemblyDescription("Вспомогательные элементы, используемые и заданиями, и сайтом")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
---
+++ GraphLabs.Tasks.Contract/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("GraphLabs.Tasks.Contract")]
+[assembly: AssemblyDescription("Контракт подключения заданий к сайту")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
